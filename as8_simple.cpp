0a7fe25fa4af8b000e04e6906872cee6ccfa1282
ladaka.tayaara <abhiventor@gmail.com>
2026-04-26 01:55:50 +0530

    removed file handling part of hashing code completely

--- as8_simple.cpp
+++ as8_simple.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <fstream>
 using namespace std;
 
 struct Student {
@@ -8,24 +7,15 @@ struct Student {
     int marks;
 };
 
-struct HashEntry {
-    int rollno;
-    int pos;
-};
-
-HashEntry h[10];
-int relt = 0;
-int s = sizeof(Student);
+Student h[10];
 
 void initHashTable() {
     for (int i = 0; i < 10; i++) {
         h[i].rollno = -1;
-        h[i].pos = -1;
     }
 }
 
 void insert_record(bool with_replacement) {
-    fstream f("student.txt", ios::binary | ios::app | ios::out);
     Student stud;
     char ch;
     do {
@@ -33,87 +23,76 @@ void insert_record(bool with_replacement) {
         cin >> stud.name >> stud.rollno >> stud.marks;
 
         int loc = stud.rollno % 10;
-        int curr_roll = stud.rollno;
-        int curr_pos = relt;
+        Student curr_stud = stud;
 
         // If using Replacement and the home location is occupied by a synonym
         if (with_replacement && h[loc].rollno != -1 && (h[loc].rollno % 10) != loc) {
-            curr_roll = h[loc].rollno;     // Demote occupant
-            curr_pos = h[loc].pos;
-            h[loc].rollno = stud.rollno;   // Place new element at its true home
-            h[loc].pos = relt;
+            curr_stud = h[loc];     // Demote occupant
+            h[loc] = stud;          // Place new element at its true home
         }
 
         // Linear probing to find the next empty slot for whichever element needs a home
         for (int i = 0; i < 10; i++) {
             int newloc = (loc + i) % 10;
             if (h[newloc].rollno == -1) {
-                h[newloc].rollno = curr_roll;
-                h[newloc].pos = curr_pos;
+                h[newloc] = curr_stud;
                 break;
             }
         }
 
-        f.write((char*)&stud, s);
-        relt++;
-
         cout << "Add more records? (y/n): ";
         cin >> ch;
     } while (ch == 'y' || ch == 'Y');
-    f.close();
 }
 
 void create_wor() { insert_record(false); }
 void create_wr()  { insert_record(true); }
 
 void display_all() {
-    fstream f("student.txt", ios::binary | ios::in);
-    Student temp;
     cout << "\nName\t\tRollNo\tMarks\n";
     cout << "--------------------------------\n";
-    while (f.read((char*)&temp, s)) {
-        cout << temp.name << "\t\t" << temp.rollno << "\t" << temp.marks << "\n";
+    for (int i = 0; i < 10; i++) {
+        if (h[i].rollno != -1) {
+            cout << h[i].name << "\t\t" << h[i].rollno << "\t" << h[i].marks << "\n";
+        }
     }
-    f.close();
 }
 
 void retrieve() {
-    fstream f("student.txt", ios::binary | ios::in);
-    Student temp;
     int keyrollno;
     cout << "Enter roll number to search: ";
     cin >> keyrollno;
-    bool found = false;
-    while (f.read((char*)&temp, s)) {
-        if (temp.rollno == keyrollno) {
+    
+    int loc = keyrollno % 10;
+    for (int i = 0; i < 10; i++) {
+        int newloc = (loc + i) % 10;
+        if (h[newloc].rollno == keyrollno) {
             cout << "Record found!\n";
-            cout << "Name: " << temp.name << "  Roll: " << temp.rollno << "  Marks: " << temp.marks << "\n";
-            found = true;
-            break;
+            cout << "Name: " << h[newloc].name << "  Roll: " << h[newloc].rollno << "  Marks: " << h[newloc].marks << "\n";
+            return;
         }
+        if (h[newloc].rollno == -1) break; // Optimization: Stop searching if empty slot found
     }
-    if (!found) {
-        cout << "Record not found.\n";
-    }
-    f.close();
+    cout << "Record not found.\n";
 }
 
 void modify() {
-    fstream f("student.txt", ios::binary | ios::in | ios::out);
-    Student temp;
     int keyrollno;
     cout << "Enter roll number to modify: ";
     cin >> keyrollno;
-    while (f.read((char*)&temp, s)) {
-        if (temp.rollno == keyrollno) {
+
+    int loc = keyrollno % 10;
+    for (int i = 0; i < 10; i++) {
+        int newloc = (loc + i) % 10;
+        if (h[newloc].rollno == keyrollno) {
             cout << "Enter new data (name rollno marks): ";
-            cin >> temp.name >> temp.rollno >> temp.marks;
-            f.seekp(-s, ios::cur);
-            f.write((char*)&temp, s);
+            cin >> h[newloc].name >> h[newloc].rollno >> h[newloc].marks;
             cout << "Record updated.\n";
+            return;
         }
+        if (h[newloc].rollno == -1) break; 
     }
-    f.close();
+    cout << "Record not found.\n";
 }
 
 int main() {
